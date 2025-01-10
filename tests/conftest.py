46adab20e148e083b17e23847f7de29c2024f114
DmitryAsl <aslamovdima10@yandex.ru>
2025-01-10 18:21:27 +0300

    add config for selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,35 @@
 import pytest
 from selenium import webdriver
-from selene import browser
+from selene import Browser, Config
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(autouse=True)
 def browser_config():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
+    # driver_options.add_argument("--headless")
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser = Browser(Config(driver))
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    driver_options.add_argument("--headless")
     browser.config.driver_options = driver_options
     browser.config.base_url = 'https://demoqa.com'
-    yield
+
+    yield browser
+
     browser.quit()
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,10 +1,13 @@
 from helpers.pages.registration_page import RegistrationPage
 from helpers.users import test_user
+import allure
 
 
 def test_registration_form():
-    registration_form = RegistrationPage()
-    registration_form.open()
-    registration_form.register(test_user)
-
-    registration_form.should_registered_user_with(test_user)
+    with allure.step('Открытие формы регистрации'):
+        registration_form = RegistrationPage()
+        registration_form.open()
+    with allure.step('Заполнение формы и отправка ее'):
+        registration_form.register(test_user)
+    with allure.step('Проверка данных регистрации'):
+        registration_form.should_registered_user_with(test_user)
